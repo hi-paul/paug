87645a134a031b23f0d15aab7ff99c702534ddda
jsca90 <mbc4805@gmail.com>
2021-06-13 21:14:15 +0900

    app, list js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
+import List from './List';
+import axios from "axios";
+
+
 
 function App() {
+  const [data, setData] =useState([]);
+  useEffect( async () => {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    // console.log(response.data);
+    setData(response.data);
+  }, []);
+  const res = data.map((item,index)=>{
+    return(<h3 key={index}>{item.title}---{item.body}</h3>)
+  })
   return (
     <div className="App">
-      gh-pages deploy
+      
+      <List data={res}/>
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function List({data}) {
+    // console.log(data);
+    return (
+        <div>
+           {data}
+        </div>
+    )
+}
+
+export default List;
